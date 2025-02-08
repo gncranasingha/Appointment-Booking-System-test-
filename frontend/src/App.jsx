f39a7214f0993ba426dd9c5dd25c47a4281032f9
gncranasingha <115582110+gncranasingha@users.noreply.github.com>
2025-02-08 17:15:32 +0530

    initialize backend and frontend parts

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+     <div className="bg-gray-100 min-h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-bold text-blue-600">
+        Hello, Tailwind CSS!
+      </h1>
+    </div>
+    </>
+  )
+}
+
+export default App
